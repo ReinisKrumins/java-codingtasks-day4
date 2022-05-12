30deeeec1ceb91a8f1ab6c0f75bd1c9ef6c1e74c
ReinisKrumins <r.krumins9@gmail.com>
2022-05-12 10:21:11 +0300

    Finished: 1st coding task

---
+++ src/firstTask/Main.java
@@ -0,0 +1,41 @@
+package firstTask;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] colorArray = {"Red", "Blue", "Green", "White", "Black", "Yellow"};
+
+        ArrayList<String> colors = new ArrayList<>(Arrays.asList(colorArray));
+
+        System.out.println("*Copied array to an ArrayList*");
+        System.out.println(colors);
+        System.out.println("*Remove 3rd element*");
+        colors.remove(2);
+        System.out.println(colors);
+        System.out.println("*Add new color at the end of list*");
+        colors.add("Orange");
+        System.out.println(colors);
+        System.out.println("*Add new color at the beginning of list*");
+        colors.add(0, "Brown");
+        System.out.println(colors);
+
+        System.out.println("*For each print out*");
+        for (String tempString : colors)
+        {
+            System.out.println(tempString);
+        }
+
+        System.out.println("*Sublist of color list*");
+        ArrayList<String> subListOfColors = new ArrayList<>(colors.subList(2, 5));
+        for (String tempString : subListOfColors)
+        {
+            System.out.println(tempString);
+        }
+
+    }
+}
